c0c4f240b4ad28f6dcb3e3d2faa3510ee6c9941e
Miguel Rivas <mrivaslezama@gmail.com>
2023-05-07 19:49:28 -0400

    base /

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://mrivaslezama.github.io/store/',
-	base: '/store',
+	base: '/',
 	integrations: [mdx(), sitemap()],
 });
