129672737c9a8b45fb75234bdfc288d20c265b98
Miguel Rivas <mrivaslezama@gmail.com>
2023-05-07 20:07:32 -0400

    netlify config again

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,8 @@
+/ astro.config.mjs
 import { defineConfig } from 'astro/config';
 import netlify from '@astrojs/netlify/functions';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 
-// https://astro.build/config
 export default defineConfig({
-	site: 'https://mrivaslezama.github.io/store/',
-	base: '/',
-	integrations: [mdx(), sitemap()],
-	output: 'server',
-	adapter: netlify(),
-  
+  output: "server",
+  adapter: netlify(),
 });
-
-
